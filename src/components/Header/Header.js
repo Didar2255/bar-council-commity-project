481f68c373dc6a2bdca2f3f423b3f1b81e6a40ef
Didar2255 <didar.a2255@gmail.com>
2021-09-26 04:10:11 +0600

    chang button icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,17 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='text-center header rounded'>
-            <nav class="navbar navbar-expand-lg navbar-dark header ">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/#">BDbar Council</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark header ">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/#">BDbar Council</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav manu-bar">
-                            <a class="nav-link active" aria-current="page" href="/home">Home</a>
-                            <a class="nav-link active" href="/member">Member of Cuncil</a>
-                            <a class="nav-link active" href="/about">About</a>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav manu-bar">
+                            <a className="nav-link active" aria-current="page" href="/home">Home</a>
+                            <a className="nav-link active" href="/member">Member of Cuncil</a>
+                            <a className="nav-link active" href="/about">About</a>
                         </div>
                     </div>
                 </div>
--- src/components/Member/Member.js
+++ src/components/Member/Member.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Member.css'
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 const Member = (props) => {
     const { img, name, address, salary, designation, job_exprience, age } = props.member
     return (
         <div className="col-md-4">
-            <div class="col card shadow border border-0">
-                <div class="card h-100 cart-body ">
-                    <img src={img} class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h4 class="card-title">Name : {name}</h4>
+            <div className="col card shadow border border-0">
+                <div className="card h-100 cart-body  ">
+                    <img src={img} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h4 className="card-title">Name : {name}</h4>
                         <h5>Desination : <small>{designation}</small></h5>
                         <p>Work Exprience : {job_exprience}</p>
                         <p>Age : {age}</p>
                         <p>Salary : <small>{salary}</small></p>
                         <p>Home Address : {address}</p>
-                        <button className='button' onClick={() => props.hendelButton(props.member)}> <FontAwesomeIcon icon={faInfoCircle} /> See Details</button>
+                        <button className='button' onClick={() => props.hendelButton(props.member)}> <FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
                     </div>
                 </div>
             </div>
